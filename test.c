78b6920ebbf349f907d1e34fb7faddcf76105ff4
mcruz <marcelo@psr-inc.com>
2025-02-18 13:43:02 -0300

    Fix

--- test.c
+++ test.c
@@ -1,55 +1,43 @@
-       _|  (     /   Amazon Linux AMI
-      ___|\___|___|
-
-https://aws.amazon.com/amazon-linux-ami/2018.03-release-notes/
-136 package(s) needed for security, out of 176 available
-Run "sudo yum update" to apply all updates.
-[root@sydney ~]# screen -dR -S Marcelo
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <string.h>
 
-#define VECTOR_SIZE 1000000000  // Tamanho total do vetor
+#define FILE_SIZE 100L * 1024L * 1024L * 1024L  // 100GB
+#define BLOCK_SIZE 1024L * 1024L * 1024L       // 1GB por processo
 
 int main(int argc, char *argv[]) {
-    int rank, size;
-    MPI_File file;
+    MPI_File fh;
     MPI_Status status;
+    int rank, size;
     MPI_Offset offset;
+    char *buf;
+    int i;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Determinar tamanho da parte de cada processo
-    int local_size = VECTOR_SIZE / size;
-    if (rank == size - 1) {
-        local_size += VECTOR_SIZE % size;  // Último processo recebe o resto
-    }
+    // Buffer para dados a serem escritos
+    buf = (char*)malloc(BLOCK_SIZE * sizeof(char));
 
-    // Alocar e inicializar vetor local
-    int *local_vector = (int *)malloc(local_size * sizeof(int));
-    for (int i = 0; i < local_size; i++) {
-        local_vector[i] = rank * 1000 + i;  // Preenchendo com valores distintos
-    }
+    // Preencher o buffer com algum valor
+    memset(buf, rank, BLOCK_SIZE);
 
-    // Abrir arquivo para escrita
-    MPI_File_open(MPI_COMM_WORLD, "output.dat", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &file);
+    // Abrir o arquivo com MPI-IO
+    MPI_File_open(MPI_COMM_WORLD, "output_file.dat", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
 
-    // Definir deslocamento baseado na posição do processo
-    offset = rank * (VECTOR_SIZE / size) * sizeof(int);
+    // Calcular o deslocamento para o início do bloco do processo atual
+    offset = rank * BLOCK_SIZE;
 
-    if(rank != 2){
-        // Escrever os dados em paralelo
-        printf("Starting Rank=%d , offset=%d\n",  rank , offset);
-        MPI_File_write_all(file, local_vector, local_size, MPI_INT, &status);
-        printf("Finish Rank=%d , offset=%d\n",  rank , offset);
-    }
-    // Fechar arquivo
-    MPI_File_close(&file);
+    // Escrever no arquivo de forma paralela
+    MPI_File_write_at(fh, offset, buf, BLOCK_SIZE, MPI_CHAR, &status);
 
-    // Liberar memória
-    free(local_vector);
+    // Fechar o arquivo
+    MPI_File_close(&fh);
+
+    // Liberar o buffer
+    free(buf);
 
     MPI_Finalize();
     return 0;
