f9498258335d08477733b5231a6b99219edbf4ce
mcruz <marcelo@psr-inc.com>
2025-02-18 13:58:40 -0300

    Fix

--- test.c
+++ test.c
@@ -3,35 +3,39 @@
 #include <mpi.h>
 #include <string.h>
 
-#define FILE_SIZE 100L * 1024L * 1024L * 1024L  // 100GB
-#define BLOCK_SIZE 1024L * 1024L * 1024L       // 1GB por processo
+#define TOTAL_FILE_SIZE 100L * 1024L * 1024L * 1024L  // 100GB
+#define BLOCK_SIZE 1024L * 1024L * 1024L       // Tamanho do bloco por processo (1GB máximo por processo)
+#define FILE_NAME "output_file.dat"
 
 int main(int argc, char *argv[]) {
     MPI_File fh;
     MPI_Status status;
     int rank, size;
     MPI_Offset offset;
+    long total_blocks_per_process;
     char *buf;
-    int i;
-
+    
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Buffer para dados a serem escritos
-    buf = (char*)malloc(BLOCK_SIZE * sizeof(char));
+    // Calcular o número total de blocos a serem escritos no arquivo
+    total_blocks_per_process = TOTAL_FILE_SIZE / size;
 
-    // Preencher o buffer com algum valor
-    memset(buf, rank, BLOCK_SIZE);
+    // Alocar o buffer de dados a ser escrito
+    buf = (char*)malloc(total_blocks_per_process * sizeof(char));
+
+    // Preencher o buffer com dados baseados no rank para distinção entre os processos
+    memset(buf, rank, total_blocks_per_process);
 
     // Abrir o arquivo com MPI-IO
-    MPI_File_open(MPI_COMM_WORLD, "output_file.dat", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+    MPI_File_open(MPI_COMM_WORLD, FILE_NAME, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
 
-    // Calcular o deslocamento para o início do bloco do processo atual
-    offset = rank * BLOCK_SIZE;
+    // Calcular o deslocamento do início do bloco para o processo atual
+    offset = rank * total_blocks_per_process;
 
     // Escrever no arquivo de forma paralela
-    MPI_File_write_at(fh, offset, buf, BLOCK_SIZE, MPI_CHAR, &status);
+    MPI_File_write_at(fh, offset, buf, total_blocks_per_process, MPI_CHAR, &status);
 
     // Fechar o arquivo
     MPI_File_close(&fh);
